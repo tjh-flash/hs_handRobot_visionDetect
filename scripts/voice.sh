ed45d74460c79bf366e411bc308f9fe3672f1e27
KongDeLiang <13674059697@163.com>
2020-07-18 15:10:58 +0800

    test over

--- scripts/voice.sh
+++ scripts/voice.sh
@@ -4,6 +4,7 @@ cd ~/catkin_ws/kdxf/Linux_aiui5.5.1059.0000_5ef07848/samples/aiui_sample/build
 source ~/.bashrc
 #启动麦克风监听
 rosrun asr_bridge asr_bridge &
+
 #启动语音过滤节点
 rosrun openni2_tracker hello &
 #启动语音智能助手
